0cebf856b104b7342c9dfcd106594c629af8e75c
Berfin Tek <tekberfin@outlook.com>
2023-04-12 00:09:01 +0300

    Login page login done

---
+++ LibraryAutomationSystem/LibraryAutomationSystem/Forms/FrmLoginScreen.Designer.cs
@@ -0,0 +1,159 @@
+﻿namespace LibraryAutomationSystem.Forms
+{
+    partial class FrmLoginScreen
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmLoginScreen));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.LLSifremiUnuttm = new System.Windows.Forms.LinkLabel();
+            this.TxtMail = new System.Windows.Forms.TextBox();
+            this.TxtSifre = new System.Windows.Forms.TextBox();
+            this.BtnGirisYap = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Ink Free", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(427, 77);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Kütüphane Otomasyon Sistemi Giriş Ekranı";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(109, 326);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(54, 25);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Mail:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(109, 374);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(58, 25);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Şifre:";
+            // 
+            // LLSifremiUnuttm
+            // 
+            this.LLSifremiUnuttm.AutoSize = true;
+            this.LLSifremiUnuttm.Location = new System.Drawing.Point(269, 402);
+            this.LLSifremiUnuttm.Name = "LLSifremiUnuttm";
+            this.LLSifremiUnuttm.Size = new System.Drawing.Size(137, 22);
+            this.LLSifremiUnuttm.TabIndex = 4;
+            this.LLSifremiUnuttm.TabStop = true;
+            this.LLSifremiUnuttm.Text = "Şifremi Unuttum";
+            this.LLSifremiUnuttm.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.LLSifremiUnuttm_LinkClicked);
+            // 
+            // TxtMail
+            // 
+            this.TxtMail.Location = new System.Drawing.Point(173, 323);
+            this.TxtMail.Name = "TxtMail";
+            this.TxtMail.Size = new System.Drawing.Size(233, 28);
+            this.TxtMail.TabIndex = 5;
+            // 
+            // TxtSifre
+            // 
+            this.TxtSifre.Location = new System.Drawing.Point(173, 371);
+            this.TxtSifre.Name = "TxtSifre";
+            this.TxtSifre.Size = new System.Drawing.Size(233, 28);
+            this.TxtSifre.TabIndex = 6;
+            // 
+            // BtnGirisYap
+            // 
+            this.BtnGirisYap.BackColor = System.Drawing.Color.ForestGreen;
+            this.BtnGirisYap.Location = new System.Drawing.Point(173, 456);
+            this.BtnGirisYap.Name = "BtnGirisYap";
+            this.BtnGirisYap.Size = new System.Drawing.Size(233, 42);
+            this.BtnGirisYap.TabIndex = 7;
+            this.BtnGirisYap.Text = "Giriş Yap";
+            this.BtnGirisYap.UseVisualStyleBackColor = false;
+            this.BtnGirisYap.Click += new System.EventHandler(this.BtnGirisYap_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(134, 101);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(187, 170);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 8;
+            this.pictureBox1.TabStop = false;
+            // 
+            // FrmLoginScreen
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 22F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(133)))), ((int)(((byte)(75)))), ((int)(((byte)(80)))));
+            this.ClientSize = new System.Drawing.Size(448, 594);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.BtnGirisYap);
+            this.Controls.Add(this.TxtSifre);
+            this.Controls.Add(this.TxtMail);
+            this.Controls.Add(this.LLSifremiUnuttm);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "FrmLoginScreen";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Login Page";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.LinkLabel LLSifremiUnuttm;
+        private System.Windows.Forms.TextBox TxtMail;
+        private System.Windows.Forms.TextBox TxtSifre;
+        private System.Windows.Forms.Button BtnGirisYap;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
---
+++ LibraryAutomationSystem/LibraryAutomationSystem/Forms/FrmLoginScreen.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace LibraryAutomationSystem.Forms
+{
+    public partial class FrmLoginScreen : Form
+    {
+        public FrmLoginScreen()
+        {
+            InitializeComponent();
+        }
+        // Data Source=DESKTOP-NBHVRA9\SQLEXPRESS;Initial Catalog=DbLibrarySystem;Integrated Security=True
+        SqlConnection conn = new SqlConnection(@"Data Source=DESKTOP-NBHVRA9\SQLEXPRESS;Initial Catalog=DbLibrarySystem;Integrated Security=True");
+
+        private void BtnGirisYap_Click(object sender, EventArgs e)
+        {
+            conn.Open();
+            SqlCommand cmd = new SqlCommand("Select * From TblKullanici Where Mail=@p1 and Sifre=@p2", conn);
+            cmd.Parameters.AddWithValue("@p1", TxtMail.Text);
+            cmd.Parameters.AddWithValue("@p2", TxtSifre.Text);
+            SqlDataReader dr = cmd.ExecuteReader();
+            if(dr.Read())
+            {
+                FrmAnaSayfa frm = new FrmAnaSayfa();
+                frm.Show();
+                this.Hide();
+            }
+            else
+            {
+                MessageBox.Show("Mail ya da şifre yanlış!", "Hata", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void LLSifremiUnuttm_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            MessageBox.Show("Şifreni hatırla!","Uyarı",MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+    }
+}
